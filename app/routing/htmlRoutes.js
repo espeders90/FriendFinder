f64927d784c1b8964ab72052596de6d7f9d038fa
Eddie Pedersen <eddiepedersen90@gmail.com>
2017-12-19 23:56:32 +0000

    survey

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,10 +8,14 @@ var path = require("path");
 
 module.exports = function(app) {
 
-    app.get("/friends", function(req, res) {
-        console.log("friends");
+    app.get("/survey", function(req, res) {
+        console.log("survey");
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
+    // app.get("/survey", function(req, res) {
+    //     console.log("survey");
+    //     res.sendFile(path.join(_dirname, ""))
+    // })
     app.get("*", function(req, res) {
         console.log("home");
         res.sendFile(path.join(__dirname, "../public/home.html"));
